2cdf0ff78cdf1caefcf64ab02a039b609f6a5536
zhao-20 <2376468125@qq.com>
2019-09-18 15:51:14 +0800

    更新2

--- src/com/atguigu/egit/test/Egit1Test.java
+++ src/com/atguigu/egit/test/Egit1Test.java
@@ -3,5 +3,7 @@ package com.atguigu.egit.test;
 public class Egit1Test {
 		public static void main(String[] args) {
 			System.out.println("aaa");
+			System.out.println("ccc");
+			System.out.println("ddd");
 		}
 }
